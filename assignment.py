0aaa3aa7cb75a4e3362e0f0ef7798af32a74ac56
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-12-11 22:43:19 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,12 +1,5 @@
 def fibonacci(n):
-    for i in range(10):
-        print(i)
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+    pass
 
 def count_digits(n):
     for i in range(10):
